1a72d4dbc3968ae0a47ca14949fd587ee377c1f8
Iliketurtle-np <88299611+Iliketurtle-np@users.noreply.github.com>
2025-03-18 23:50:52 +0800

    installed axios

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const multer = require('multer');
+const axios = require('axios'); // Import axios
+
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
